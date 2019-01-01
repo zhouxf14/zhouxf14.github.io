1ab45202cf2e0827dbcb04c35854a8909fe54ff2
zhouxf14 <zhouxf14@163.com>
2018-12-31 19:01:55 -0500

    canhai suggestion refine

--- js/index.js
+++ js/index.js
@@ -2,25 +2,12 @@
   	$("#all").addClass("active");
 
   	$(".tag").click(function() {
-  		// $(this).toggleClass("active");
-  		// if(($(this).attr("id") != "all") && ($("#all").hasClass("active"))) {
-  		// 	$("#all").removeClass("active");
-  		// }
-
-  		if($(this).attr("id") == "all") {
-  			$(".single").removeClass("active");
-  			$("#all").addClass("active");
-  		} else {
-  			// console.log("test");
-  			$(this).toggleClass("active");
-  			if(($("#all").hasClass("active"))) {
-  				$("#all").removeClass("active");
-  			} else {
-  				if (!$(".single").hasClass("active")) {
-  					$("#all").addClass("active");
-  				}
-  			}
-  		}
+      if($(this).hasClass("active")) {
+        $(this).removeClass("active");
+      } else {
+        $(".tag").removeClass("active");
+        $(this).addClass("active");
+      }
   	});
 
   	$(".cv-btn").click(function() {
